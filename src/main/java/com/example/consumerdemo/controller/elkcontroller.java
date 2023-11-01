82565cd1fe1135c38781f25c7d8ab9ad5da42ae1
Syed Ateebul Islam <32341313+syedateebulislam@users.noreply.github.com>
2023-11-02 00:29:39 +0530

    test commit

---
+++ src/main/java/com/example/consumerdemo/controller/elkcontroller.java
@@ -0,0 +1,18 @@
+package com.example.consumerdemo.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class elkcontroller {
+
+    @GetMapping("/logs")
+    public String getEmployee(){
+
+        log.info("Method startLogging started");
+        return "test";
+    }
+
+}
